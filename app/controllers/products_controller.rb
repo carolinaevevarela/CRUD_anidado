277b1c4a8f9b51534f89c414c29fdfcffa979234
Carolina <carolinaevevarela@gmail.com>
2019-06-17 21:55:59 -0400

    formulario sin mostrar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,13 @@ class ProductsController < ApplicationController
     @product.save
     redirect_to category_path(params[:category_id])
   end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to @product.category
+  end
+
   def product_params
     params.require(:product).permit(:name, :price)
   end
